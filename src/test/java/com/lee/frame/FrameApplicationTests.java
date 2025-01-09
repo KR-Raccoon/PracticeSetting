dfe9a9a4a50df97207c1ad68036cd40b91cc610b
LeeSJ <zzangyi2@gmail.com>
2025-01-09 10:35:36 +0900

    초기 셋팅 커밋

---
+++ src/test/java/com/lee/frame/FrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.frame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
